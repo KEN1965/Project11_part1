9f2e848d2b19b4b62d447d9c5ef74dc46cdc4fe1
Kenichi Takahama <hammer528@msn.com>
2023-01-09 09:19:10 +0900

    project11_part1「Accepting multi-line text input with TextEditor」finish

--- Project11_part1/ContentView.swift
+++ Project11_part1/ContentView.swift
@@ -7,36 +7,17 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @State var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(
-            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
-        )
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    //Project11_part1 やっていきやしょう(๑>◡<๑)
-    //Toogleボタンの作成
-    //カスタムしていきます
-    @State private var remenberMe = false
+    //Project11_part1 [Accepting multi-line text input with TextEditor]
+    //やっていきやしょう(๑>◡<๑)
+    //TextEditorで複数行のテキスト入力
+    @AppStorage("notes") private var notes = ""
+    
     var body: some View {
-        VStack{
-            PushButton(title: "Remenber Me", isOn: remenberMe)
-            Text(remenberMe ? "On" : "Off")
+        NavigationView {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
     }
 }
