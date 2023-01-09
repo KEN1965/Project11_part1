04fe660d464fbb21c5f703852930b871b63d5fe6
Kenichi Takahama <hammer528@msn.com>
2023-01-09 09:11:31 +0900

    project11_part1「Creating a custom component with @Binding」finish

--- Project11_part1/ContentView.swift
+++ Project11_part1/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @State var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+        .background(
+            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
+        )
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    //Project11_part1 やっていきやしょう(๑>◡<๑)
+    //Toogleボタンの作成
+    //カスタムしていきます
+    @State private var remenberMe = false
+    var body: some View {
+        VStack{
+            PushButton(title: "Remenber Me", isOn: remenberMe)
+            Text(remenberMe ? "On" : "Off")
+        }
     }
 }
 
